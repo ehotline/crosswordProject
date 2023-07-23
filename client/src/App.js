2cf0cfd5beabf2078c35893748477e94cbf19b6b
Dmitry <eliseenkov.d@yandex.ru>
2023-07-23 11:53:41 +0300

    tab change

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,9 @@
 function App() {
-  return (
-    <div className="App">
-      WORKING
-    </div>
-  );
+    return (
+        <div className="App">
+            WORKING
+        </div>
+    );
 }
 
 export default App;
