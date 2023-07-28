a7dc8d48bcf06ae3427880f5721623d035779c3d
Dmitry <eliseenkov.d@yandex.ru>
2023-07-28 15:12:12 +0300

    Рефакторинг структуры проекта

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,8 @@
 import React from 'react'
 import { BrowserRouter } from 'react-router-dom'
 import AppRouter from './components/AppRouter';
-import '../src/styles/App.css'
-import Header from './components/Header';
+import './App.css'
+import Header from './components/Header/Header';
 
 function App() {
     return (
